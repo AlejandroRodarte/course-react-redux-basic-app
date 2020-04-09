ec74d65b00793d2e8abc4615ded279763aaaa84f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-09 10:20:26 -0600

    Dispatching Actions

--- redux-basics.js
+++ redux-basics.js
@@ -10,6 +10,18 @@ const rootReducer = (state = initialState, action) => {
 
     switch (action.type) {
 
+        case 'INC_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+
+        case 'ADD_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + action.payload.value
+            };
+
         default:
             return state;
 
@@ -18,5 +30,20 @@ const rootReducer = (state = initialState, action) => {
 };
 
 const store = createStore(rootReducer);
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'INC_COUNTER'
+});
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'ADD_COUNTER',
+    payload: {
+        value: 10
+    }
+});
 
 console.log(store.getState());
