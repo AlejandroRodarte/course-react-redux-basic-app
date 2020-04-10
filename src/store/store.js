d8dc010228204905fd14e6c6d1d0c45c34bd5afe
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-10 15:42:46 -0600

    Adding Middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import counterReducer from './reducers/counter';
 import resultsReducer from './reducers/results';
@@ -8,4 +8,20 @@ const rootReducer = combineReducers({
     results: resultsReducer
 });
 
-export default createStore(rootReducer);
+// 3rd order function: called directly by redux, passing the entire store as argument
+// 2nd order function: called with a next function argument
+// 1st order function: called with the currently dispatched action, must return with a call to next
+// with the action object to indicate we want to move further in the middleware chain
+// of course you can mutate in the middleware the action object
+const logger = store => next => action => {
+
+    console.log('[Middleware] Dispatching', action);
+
+    const result = next(action);
+    console.log('[Middleware] Next State', store.getState());
+
+    return result;
+
+};
+
+export default createStore(rootReducer, applyMiddleware(logger));
