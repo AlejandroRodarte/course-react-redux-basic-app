3970d38938289a520b114453f96de83cddd3ab45
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-10 17:07:06 -0600

    Using Action Creators and Get State

--- src/store/actions/results.js
+++ src/store/actions/results.js
@@ -1,6 +1,9 @@
 import * as ResultsTypes from '../types/results';
 
-export const startStoreResult = (result) => (dispatch) => setTimeout(() => dispatch(storeResult(result)), 2000);
+export const startStoreResult = (result) => (dispatch, getState) => setTimeout(() => {
+    console.log(getState().counter.counter);
+    dispatch(storeResult(result));
+}, 2000);
 
 export const storeResult = (result) => ({
     type: ResultsTypes.STORE_RESULT,
